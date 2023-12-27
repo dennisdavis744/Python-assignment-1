20289c80044296d6cc90448732e111ed314483be
dennisdavis744 <139228700+dennisdavis744@users.noreply.github.com>
2023-12-27 23:07:36 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,5 @@
+year=int(input("Enter the year :"))
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("The given year is a leap year!")
+else:
+    print("The given year isn't a leap year!")
---
+++ odd.py
@@ -0,0 +1,5 @@
+num1=int(input('enter num1 :'))
+if(num1%2)==0:
+    print('the given num1 is even')
+else:
+    print('the given num1 is odd')
